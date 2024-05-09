8afdce9b0385b8bae63336565832ae6a0bbecc28
Fabricio Menezes <fabricio-menezes@live.com>
2024-05-09 21:01:46 +0100

    ADD all project

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,34 @@
-console.log("hello world");
+AOS.init();
+
+var currentYear = new Date().getFullYear();
+var eventDate = new Date(`Jun 29, ${currentYear} 00:00:00`);
+
+function updateEventDate() {
+    const currentDate = new Date();
+    
+    if (currentDate >= eventDate) {
+        currentYear++;
+        eventDate = new Date(`Jun 29, ${currentYear} 00:00:00`);
+    }
+}
+
+const timer = setInterval( function() {
+    updateEventDate();
+
+    const currentDate = new Date();
+    const eventTimeStamp = eventDate.getTime();
+    const currentTimeStamp = currentDate.getTime();
+
+    const timeUntilEvent = eventTimeStamp - currentTimeStamp;
+
+    const dayMS = 1000 * 60 * 60 * 24;
+    const hourMS = 1000 * 60 * 60;
+    const minuteMS = 1000 * 60;
+
+    const daysUntilEvent = Math.floor(timeUntilEvent / dayMS);
+    const hoursUntilEvent = Math.floor((timeUntilEvent % dayMS) / hourMS);
+    const minutesUntilEvent = Math.floor((timeUntilEvent % hourMS) / minuteMS);
+    const secondsUntilEvent = Math.floor((timeUntilEvent % minuteMS) / 1000);
+
+    document.getElementById('counter').innerHTML = `${daysUntilEvent}d ${hoursUntilEvent}h ${minutesUntilEvent}m ${secondsUntilEvent}s`;
+}, 1000);
